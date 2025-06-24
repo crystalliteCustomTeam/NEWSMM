d360812cf1750f08575638464993fdc1fcd39368
Harry Kennedy <72253270+Harrythedevelopercs@users.noreply.github.com>
2025-06-25 00:32:08 +0500

    chat added

--- src/app/layout.js
+++ src/app/layout.js
@@ -36,17 +36,26 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
 
-     
-       <GoogleTagManager gtmId="GTM-KQFVWWGF" />
-     
+
+      <GoogleTagManager gtmId="GTM-KQFVWWGF" />
+      <Script id="Chat">
+        {`
+        
+    window.__lc = window.__lc || {};
+    window.__lc.license = 19024239;
+    window.__lc.integration_name = "manual_channels";
+    window.__lc.product_name = "livechat";
+    ;(function(n,t,c){function i(n){return e._h?e._h.apply(null,n):e._q.push(n)}var e={_q:[],_h:null,_v:"2.0",on:function(){i(["on",c.call(arguments)])},once:function(){i(["once",c.call(arguments)])},off:function(){i(["off",c.call(arguments)])},get:function(){if(!e._h)throw new Error("[LiveChatWidget] You can't use getters before load.");return i(["get",c.call(arguments)])},call:function(){i(["call",c.call(arguments)])},init:function(){var n=t.createElement("script");n.async=!0,n.type="text/javascript",n.src="https://cdn.livechatinc.com/tracking.js",t.head.appendChild(n)}};!n.__lc.asyncInit&&e.init(),n.LiveChatWidget=n.LiveChatWidget||e}(window,document,[].slice))
+`}
+      </Script>
       <body className={`${fontSans.variable} antialiased font-sans font-normal`}>
-       
-      <Header />
-      <main>
-        {children}
-      </main>
-      <Footer />
-    </body>
+
+        <Header />
+        <main>
+          {children}
+        </main>
+        <Footer />
+      </body>
     </html >
   )
 }
