d4a5b779909768abd38dc5e8f5b3f27e7f8a034e
Harry Kennedy <72253270+Harrythedevelopercs@users.noreply.github.com>
2025-06-24 05:19:05 +0500

    live chat added

--- src/app/layout.js
+++ src/app/layout.js
@@ -36,29 +36,9 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
 
-      <Script id="tawk" strategy="afterInteractive">
-        {`
-          var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-          (function(){
-          var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-          s1.async=true;
-          s1.src='https://embed.tawk.to/6765b309af5bfec1dbdf1b02/1ifik6nvo';
-          s1.charset='UTF-8';
-          s1.setAttribute('crossorigin','*');
-          s0.parentNode.insertBefore(s1,s0);
-          })();
-        `}
-      </Script>
+     
        <GoogleTagManager gtmId="GTM-KQFVWWGF" />
-      {/* <Script id="GTMCODE" strategy="beforeInteractive">
-        {`
-        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-        'https://www.googletagmanager.com/gtm.j...efore(j,f);
-        })(window,document,'script','dataLayer','GTM-KQFVWWGF');
-        `}
-      </Script> */}
+     
       <body className={`${fontSans.variable} antialiased font-sans font-normal`}>
        
       <Header />
